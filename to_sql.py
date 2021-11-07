9bd37c1c319fc5113f4cc2e3efcd7bc199fa621b
RamonHA <ramon1001@live.com.mx>
2021-11-07 22:52:34 +0000

    to_sql creation

---
+++ to_sql.py
@@ -0,0 +1,6 @@
+# We are going to push registers to a postgres sql database
+
+# This is a test file for the sql connection
+# Later on, we are going to make the final changes to fastfood script
+
+
